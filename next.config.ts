3210f15e4bd5e4860a709d0402f72e6b802799b6
Aman Bhandari <150219612+Aman37Bhandari@users.noreply.github.com>
2025-12-08 11:47:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,32 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
   images: {
-    unoptimized: process.env.NEXT_PUBLIC_DEPLOYMENT === "development",
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "i.pinimg.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
+    domains: [
+      'images.unsplash.com',
+      'i.pravatar.cc',
+      'i.pinimg.com',
+      'api.dicebear.com',
+      'placehold.co'
     ],
   },
 };
