b23653c77874471a39c5fc2ad6179a65a79429d3
Aman Bhandari <150219612+Aman37Bhandari@users.noreply.github.com>
2026-01-05 12:13:23 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,75 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+import globalConfig from "@/config/globalConfig";
 
+const nextConfig: NextConfig = {
+  // cacheComponents: true,
+  reactStrictMode: true,
+  reactCompiler: false,
   images: {
-    domains: [
-      'images.unsplash.com',
-      'i.pravatar.cc',
-      'i.pinimg.com',
-      'api.dicebear.com',
-      'placehold.co'
+    unoptimized: globalConfig.deployment.development,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.astroverse.in",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3002",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "blogs-api.astroverse.in",
+        port: "",
+        pathname: "/**",
+      },
     ],
+  },
+  async headers() {
+    return globalConfig.deployment.production
+      ? [
+          {
+            source: "/static/:path*",
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=31536000, immutable",
+              },
+            ],
+          },
+          {
+            source: "/(.*)",
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=0, must-revalidate",
+              },
+            ],
+          },
+        ]
+      : [];
   },
 };
 
